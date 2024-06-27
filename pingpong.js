d7cfdc424cef0e76511254635c9f2311f22ef6ee
Ana Julia <ana.cerisoli@escola.pr.gov.br>
2024-06-27 17:41:10 -0300

    cria bolinha

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,8 @@
+//aqui eu vou criar a bolinha 
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -7,4 +12,11 @@ function draw(){
     //aqui vou por a cor da "mesa""
     //A cor da mesa é em rgb
     background(255,20,147);
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha) ;
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha, tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha) ;
 }
